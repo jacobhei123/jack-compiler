144f37860f618ae27f8f57eb8dcd4b4feb761669
jacobhei123 <helio.pinhozone@gmail.com>
2024-02-08 13:34:02 -0300

    primeiro commit

---
+++ src/main/java/br/ufma/ecp/App.java
@@ -0,0 +1,69 @@
+package br.ufma.ecp;
+
+import br.ufma.ecp.token.Token;
+import static br.ufma.ecp.token.TokenType.EOF; 
+
+public class App 
+{
+
+    
+    public static void main( String[] args )
+    {
+
+    
+        String input = """
+            // Define uma função que retorna o n-ésimo número de Fibonacci.
+            function int fibonacci(int n) {
+            var int a, b, temp, i;
+            let a = 0;
+            let b = 1;
+            if (n <= 0) {
+                return 0;
+            } else if (n == 1) {
+                return 1;
+            } else {
+                for (let i = 2; i <= n; i = i + 1) {
+                    let temp = b;
+                    let b = a + b;
+                    let a = temp;
+                }
+                return b;
+            }
+}
+
+// Função principal que imprime os primeiros 10 números de Fibonacci.
+function void main() {
+    var int i;
+    for (let i = 0; i < 10; i = i + 1) {
+        do Output.printInt(fibonacci(i));
+    }
+}
+                
+                """;
+        Scanner scan = new Scanner (input.getBytes());
+        for (Token tk = scan.nextToken(); tk.type != EOF; tk = scan.nextToken()) {
+            System.out.println(tk);
+        }
+
+        /*
+        Parser p = new Parser (input.getBytes());
+        p.parse();
+        */
+
+
+        //Parser p = new Parser (fromFile().getBytes());
+        //p.parse();
+
+        /*
+        String input = "489-85+69";
+        Scanner scan = new Scanner (input.getBytes());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        Token tk = new Token(NUMBER, "42");
+        System.out.println(tk);
+        */
+    }
+}
---
+++ src/main/java/br/ufma/ecp/token/TokenType.java
@@ -0,0 +1,55 @@
+package br.ufma.ecp.token;
+
+import java.util.List;
+
+public enum TokenType {
+
+     // Literals.
+     NUMBER,
+     STRING,
+
+
+     IDENT,
+
+ 
+     // keywords
+     WHILE, CLASS,CONSTRUCTOR,FUNCTION,
+    METHOD,FIELD,STATIC,VAR,INT,
+    CHAR,BOOLEAN,VOID,TRUE,FALSE,
+    NULL,THIS,LET,DO,IF,ELSE, RETURN, PRINTLN,
+
+     EOF,
+
+     ILLEGAL,
+
+     // symbols
+    LPAREN,RPAREN,
+    LBRACE, RBRACE,
+    LBRACKET,RBRACKET,
+
+    COMMA, SEMICOLON, DOT,
+  
+    PLUS,  MINUS,ASTERISK, SLASH,
+
+    AND, OR, NOT,
+
+    LT, GT, EQ;
+
+     static public boolean isSymbol (char c) {
+        String symbols = "{}()[].,;+-*/&|<>=~";
+        return symbols.indexOf(c) > -1;
+    }
+
+
+    static public boolean isKeyword (TokenType type) {
+        List<TokenType> keywords  = 
+            List.of(
+                WHILE, CLASS,CONSTRUCTOR,FUNCTION,
+                METHOD,FIELD,STATIC,VAR,INT,
+                CHAR,BOOLEAN,VOID,TRUE,FALSE,
+                NULL,THIS,LET,DO,IF,ELSE, RETURN, PRINTLN
+            );
+            return keywords.contains(type);
+    }
+
+}
